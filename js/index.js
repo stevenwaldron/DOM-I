58cb35f9762c28ce07a661e6f2868d40ace30d28
Steven Waldron <stevenwaldron@Stevens-MBP.home>
2020-01-16 23:44:43 -0500

    MVP

--- js/index.js
+++ js/index.js
@@ -40,3 +40,73 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+let nav = document.querySelectorAll("a");
+nav[0].textContent = siteContent["nav"]["nav-item-1"];
+nav[1].textContent = siteContent["nav"]["nav-item-2"];
+nav[2].textContent = siteContent["nav"]["nav-item-3"];
+nav[3].textContent = siteContent["nav"]["nav-item-4"];
+nav[4].textContent = siteContent["nav"]["nav-item-5"];
+nav[5].textContent = siteContent["nav"]["nav-item-6"];
+
+
+let ctaImg = document.getElementById("cta-img");
+ctaImg.setAttribute('src', siteContent["cta"]["img-src"]);
+
+let ctaText = document.querySelector("h1");
+ctaText.textContent = siteContent["cta"]["h1"]; 
+
+let ctaButton = document.querySelector("button");
+ctaButton.textContent = siteContent['cta']["button"];
+
+let h4 = document.querySelectorAll("h4");
+h4[0].textContent = siteContent["main-content"]["features-h4"];
+h4[1].textContent = siteContent["main-content"]["about-h4"];
+h4[2].textContent = siteContent["main-content"]["services-h4"];
+h4[3].textContent = siteContent["main-content"]["product-h4"];
+h4[4].textContent = siteContent["main-content"]["vision-h4"];
+h4[5].textContent = siteContent["contact"]["contact-h4"];
+
+let p = document.querySelectorAll("p");
+p[0].textContent = siteContent["main-content"]["features-content"];
+p[1].textContent = siteContent["main-content"]["about-content"];
+p[2].textContent = siteContent["main-content"]["services-content"];
+p[3].textContent = siteContent["main-content"]["product-content"];
+p[4].textContent = siteContent["main-content"]["vision-content"];
+p[5].textContent = siteContent["contact"]["address"];
+p[6].textContent = siteContent["contact"]["phone"];
+p[7].textContent = siteContent["contact"]["email"];
+p[8].textContent = siteContent["footer"]["copyright"];
+
+
+let middleImg = document.getElementById("middle-img");
+middleImg.setAttribute('src',siteContent['main-content']["middle-img-src"]);
+
+const forum = document.createElement("a");
+forum.setAttribute("href","#");
+
+forum.textContent = "Forum";
+forum.style.color = "green";
+
+let navBar = document.querySelector("nav");
+navBar.appendChild(forum);
+
+const more = document.createElement("a");
+more.setAttribute("href","#")
+
+more.textContent = "More";
+more.style.color = "green";
+
+navBar.appendChild(more);
+
+nav[0].style.color = "green";
+nav[1].style.color = "green";
+nav[2].style.color = "green";
+nav[3].style.color = "green";
+nav[4].style.color = "green";
+nav[5].style.color = "green";
+nav[6].style.color = "green";
+nav[7].style.color = "green";
+
+
